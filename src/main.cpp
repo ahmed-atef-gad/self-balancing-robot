434fc9f6c60b5ddca2a8e041ca393b4551b37aaa
ahmedelazky <ahmed.elzaki291@gmail.com>
2025-11-28 17:50:53 +0200

    feat: add watchdog timer to prevent stuck

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <MPU6050_tockn.h>
 #include <Wire.h>
+#include <avr/wdt.h>
 
 // Motor control pins
 #define enA 3
-#define in1 5
-#define in2 4
+#define in1 4
+#define in2 5
 
 #define enB 9
 #define in3 11
@@ -16,16 +17,18 @@
 #define BACKWARD -1
 #define STOP 0
 
+#define LED 12
+
 // Debugging flag
-#define Debugging 1
+#define Debugging 0
 
 // MPU6050 sensor object
 MPU6050 mpu6050(Wire);
 
 // PID parameters
-double Kp = 70.0; // Proportional constant
-double Ki = 1.5;  // Integral constant
-double Kd = 3.0;  // Derivative constant
+double Kp = 40.0; // Proportional constant
+double Ki = 0.25;  // Integral constant
+double Kd = 3.5;  // Derivative constant
 double output = 0;
 double angleY = 0;
 double setpoint = 0;       // Target angle (upright position)
@@ -42,11 +45,13 @@ void motorControl(float speed, int direction);
 
 void setup()
 {
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
   // Initialize serial communication if debugging is enabled
-  #if Debugging == 1
-    Serial.begin(9600);
-    Serial.println("Initializing>>>>");
-  #endif
+#if Debugging == 1
+  Serial.begin(9600);
+  Serial.println("Initializing>>>>");
+#endif
 
   // Initialize I2C communication
   Wire.begin();
@@ -63,21 +68,42 @@ void setup()
   analogWrite(enA, 100);
   analogWrite(enB, 100);
 
+  for (uint8_t i = 0; i < 5; i++)
+  {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+  }
+
+
   // Initialize the MPU6050 sensor
   initMpu();
-  
+
   // Set the target angle to the current Y angle
   setpoint = mpu6050.getAngleY();
+  filteredAngleY = setpoint;
 
   // Print the setpoint if debugging is enabled
-  #if Debugging == 1
-    Serial.print("Setpoint: ");
-    Serial.println(setpoint);
-  #endif
+#if Debugging == 1
+  Serial.print("Setpoint: ");
+  Serial.println(setpoint);
+#endif
+  digitalWrite(LED, HIGH);
+
+  // Enable watchdog timer with 1 second timeout to prevent  MPU6050 stucking issue
+  wdt_enable(WDTO_1S);
+
 }
 
 void loop()
 {
+  // Reset watchdog timer
+  wdt_reset();
+
+#if Debugging == 0
+  delay(10); // Loop delay for stability
+#endif
   // Update MPU6050 sensor data
   mpu6050.update();
   float accelAngleY = mpu6050.getAccAngleY();
@@ -100,14 +126,16 @@ void loop()
   integral = constrain(integral, -60, 60); // Constrain integral to prevent windup
   float derivative = Kd * (error - previousError) / elapsedTime;
   float output = Kp * error + integral + derivative;
+  output = constrain(output, -255, 255); // Constrain output to motor PWM range
 
   previousError = error;
 
   // Motor control based on PID output
-  if (abs(setpoint - angleY) < 1.5)
-  { // Deadband of ±1.5 degrees
+  if (abs(setpoint - angleY) < 2 || error >= 45 || error <= -45)
+  { // Deadband of ±2 degrees and safety cutoff at ±45 degrees
     motorControl(0, STOP);
     integral = 0; // Reset integral term when within deadband
+    output = 0;
   }
   else if (output > 0)
   {
@@ -119,14 +147,17 @@ void loop()
   }
 
   // Print debugging information if debugging is enabled
-  #if Debugging == 1
-    Serial.print("AngleY: ");
-    Serial.print(angleY);
-    Serial.print(" | PID Output: ");
-    Serial.println(output);
-    Serial.print("error: ");
-    Serial.println(error);
-  #endif
+#if Debugging == 1
+  // output in volt
+  output = output * (5.0 / 255.0);
+  if (output < 0) output = -output;
+  Serial.print("   AngleY:");
+  Serial.print(angleY);
+  Serial.print("   Setpoint:");
+  Serial.print(setpoint);
+  Serial.print("   Output(V):");
+  Serial.println(output);
+#endif
 }
 
 void motorControl(float speed, int direction)
@@ -134,11 +165,6 @@ void motorControl(float speed, int direction)
   // Constrain motor speed to valid PWM range (0-255)
   int motorSpeed = constrain(speed, 20, 255);
 
-  // Print motor speed if debugging is enabled
-  #if Debugging == 1
-    Serial.print("Speed: ");
-    Serial.println(motorSpeed);
-  #endif
 
   // Control motors based on direction
   if (direction == FORWARD)
@@ -181,7 +207,7 @@ void initMpu()
   mpu6050.calcGyroOffsets(true); // Calculate gyro offsets
 
   // Print a message if debugging is enabled
-  #if Debugging == 1
-    Serial.println("\nRobot is ready to go  (●'◡'●)");
-  #endif
+#if Debugging == 1
+  Serial.println("\nRobot is ready to go  (●'◡'●)");
+#endif
 }
